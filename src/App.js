e90ad315aed040ad21f9f89f40126f0792404349
WahyuS002 <syahputrawahyu61@gmail.com>
2022-02-20 18:15:50 +0700

    :art: disabled increment / decrement amount

--- src/App.js
+++ src/App.js
@@ -153,7 +153,7 @@ function App() {
                                         decrementMintAmount()
                                     }}
                                 >
-                                    <LeftButton />
+                                    <LeftButton color={canDecrementAmount ? '#FAAE66' : '#FFEBD9'} />
                                     <span className="absolute top-6 right-9">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
@@ -171,7 +171,7 @@ function App() {
                                         incrementMintAmount()
                                     }}
                                 >
-                                    <RightButton />
+                                    <RightButton color={canIncrementAmount ? '#FAAE66' : '#FFEBD9'} />
                                     <span className="absolute top-6 right-9">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
