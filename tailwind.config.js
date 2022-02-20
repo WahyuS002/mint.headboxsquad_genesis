368722448143836b1c53eddfd66ec5c11ee00909
WahyuS002 <syahputrawahyu61@gmail.com>
2022-02-20 15:53:54 +0700

    :tada: fresh cra + tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                grandstander: 'Grandstander',
+                inter: 'Inter',
+            },
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': {
+                        transform: 'rotate(-3deg)',
+                    },
+                    '50%': {
+                        transform: 'rotate(3deg)',
+                    },
+                },
+            },
+            animation: { wiggle: 'wiggle 1s ease-in-out infinite' },
+        },
+    },
+    plugins: [],
+}
