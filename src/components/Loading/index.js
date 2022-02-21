d2e74df262a865c5b73a740be05f822a71eaa666
WahyuS002 <syahputrawahyu61@gmail.com>
2022-02-21 21:43:51 +0700

    :zap: added draggable attribute on img element

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -13,16 +13,16 @@ import box from '../../assets/Loading/box.svg'
 export default function Loading() {
     return (
         <div className="relative">
-            <img className="absolute -left-5 bottom-4 animate-heart-beat animate-up-and-down" src={leftLove} alt="" />
-            <img className="absolute -top-4 right-2 animate-heart-beat animate-up-and-down" src={middleLove} alt="" />
-            <img className="absolute -right-5 bottom-4 animate-heart-beat animate-up-and-down" src={rightLove} alt="" />
-            <img src={box} className="animate-up-and-down" alt="" />
-            <img className="absolute animate-up-and-down animation-delay-100 left-0 -bottom-3" src={smallStar} alt="" />
-            <img className="absolute animate-up-and-down animation-delay-500 -left-3 top-3" src={mediumStar} alt="" />
-            <img className="absolute animate-up-and-down animation-delay-100 left-4 -top-3" src={mediumStar} alt="" />
-            <img className="absolute animate-up-and-down animation-delay-500 -right-3 top-3" src={largeStar} alt="" />
-            <img className="absolute animate-up-and-down animation-delay-500 -right-3 bottom-0" src={largeStar} alt="" />
-            <img className="absolute animate-up-and-down animation-delay-500 right-4 -bottom-3" src={smallStar} alt="" />
+            <img draggable={false} className="absolute -left-5 bottom-4 animate-heart-beat animate-up-and-down" src={leftLove} alt="" />
+            <img draggable={false} className="absolute -top-4 right-2 animate-heart-beat animate-up-and-down" src={middleLove} alt="" />
+            <img draggable={false} className="absolute -right-5 bottom-4 animate-heart-beat animate-up-and-down" src={rightLove} alt="" />
+            <img draggable={false} src={box} className="animate-up-and-down" alt="" />
+            <img draggable={false} className="absolute animate-up-and-down animation-delay-100 left-0 -bottom-3" src={smallStar} alt="" />
+            <img draggable={false} className="absolute animate-up-and-down animation-delay-500 -left-3 top-3" src={mediumStar} alt="" />
+            <img draggable={false} className="absolute animate-up-and-down animation-delay-100 left-4 -top-3" src={mediumStar} alt="" />
+            <img draggable={false} className="absolute animate-up-and-down animation-delay-500 -right-3 top-3" src={largeStar} alt="" />
+            <img draggable={false} className="absolute animate-up-and-down animation-delay-500 -right-3 bottom-0" src={largeStar} alt="" />
+            <img draggable={false} className="absolute animate-up-and-down animation-delay-500 right-4 -bottom-3" src={smallStar} alt="" />
         </div>
     )
 }
